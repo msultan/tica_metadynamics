a451d22f1aba4783930bf56795171a9114152bed
msultan <msultan@stanford.edu>
2017-03-08 11:16:59 -0800

    more changes

--- tica_metadynamics/post_process.py
+++ tica_metadynamics/post_process.py
@@ -1,11 +1,11 @@
 #!/bin/env python
 from msmbuilder.utils import load,dump
-import os,glob,shutil
-from mdtraj.utils import enter_temp_directory
+import os,glob
 from subprocess import call
 from multiprocessing import Pool
 from .utils import concatenate_folder
 from .plumed_writer import get_plumed_dict
+from jinga2 import Template
 
 def process_folder(job_tuple):
     r1, r2, script = job_tuple
@@ -13,16 +13,19 @@ def process_folder(job_tuple):
     print(fname)
     base_dir = os.getcwd()
     os.chdir(os.path.join(base_dir,fname))
-    traj_file_loc = os.path.abspath("tic_%s/tic_%s.xtc"%(r2,r2))
-    bias_file_loc = os.path.abspath("tic_%s/%s_%s.bias"%(r1,r1,r2))
-    with enter_temp_directory():
-        f = open("./plumed.dat",'w')
-        f.writelines(script)
-        f.close()
-        cmd = ["plumed","--no-mpi", "driver", "--mf_xtc", traj_file_loc]
-        ret_code = call(cmd)
-        shutil.copy("%s_%s.bias"%(r1,r2),bias_file_loc)
-        print(ret_code)
+    try:
+        os.remove("r%s_t%s.bias"%(r1,r2))
+    except:
+        pass
+    traj_file_loc = os.path.abspath("../tic_%s/tic_%s.xtc"%(r2,r2))
+    plumed_file = "./plumed_reweight_%d_%d.dat"%(r1,r2)
+    print(traj_file_loc,plumed_file)
+    f = open(plumed_file,'w')
+    f.writelines(script)
+    f.close()
+    cmd = ["plumed","--no-mpi", "driver", "--mf_xtc", traj_file_loc, "--plumed" ,plumed_file]
+    ret_code = call(cmd)
+    print(ret_code)
     os.chdir(base_dir)
     return
 
@@ -37,13 +40,12 @@ def process_all_replicas(file_loc,redo=True):
     sim_mdl.pace = 1000000000
     sim_mdl.height = 0
     sim_mdl.stride = 1
-    sim_mdl.bias_file ="\%s"
+    sim_mdl.bias_file ="{{fname}}"
+    plumed_scripts_dict = get_plumed_dict(sim_mdl)
     full_dict={}
     for r1 in range(sim_mdl.n_tics):
         for r2 in range(sim_mdl.n_tics):
-            sim_mdl.bias_file ="r%s_r%s.bias"%(r1,r2)
-            plumed_scripts_dict = get_plumed_dict(sim_mdl)
-            full_dict["%d_%d"%(r1,r2)] = plumed_scripts_dict[r1]
+            full_dict["%d_%d"%(r1,r2)] = Template(plumed_scripts_dict[r1]).render(fname="r%d_t%d.bias"%(r1,r2))
 
     p = Pool(n_tics)
     jobs =[(r1, r2, full_dict["%d_%d"%(r1,r2)]) for r1 in range(sim_mdl.n_tics)
