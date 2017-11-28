6835c4218090b15d8b6e40ffd15c9f1a9bb73f16
msultan <msultan@stanford.edu>
2017-11-28 14:29:42 -0800

    work around to make travis stop emailing me

--- tica_metadynamics/pyplumed/__init__.py
+++ tica_metadynamics/pyplumed/__init__.py
@@ -1,4 +1,7 @@
 from .render_df import render_df
 from .render_meta import render_metad_code, render_metad_bias_print
-from .render_network import render_network
 from .render_tics import render_tic, render_tic_wall
+try:
+    from .render_network import render_network
+except:
+    pass
